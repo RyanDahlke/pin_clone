71f0b708b3880105f8bfd52ce366e5e70970ecbd
Ryan <ryandahlke@gmail.com>
2014-12-29 13:38:45 -0600

    add html

--- pin_clone.py
+++ pin_clone.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask.ext.restless import APIManager
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, Text
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pin.db'
 db = SQLAlchemy(app)
 
@@ -14,7 +15,7 @@ class Pin(db.Model):
 
 db.create_all()
 
-api_manager = APIManager (app, flask_sqlalchemy_db=db)
+api_manager = APIManager(app, flask_sqlalchemy_db=db)
 api_manager.create_api(Pin, methods= ['GET', 'POST', 'DELETE', 'PUT'])
 
 @app.route('/')
