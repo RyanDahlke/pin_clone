da5fcc2c2b6a75e38eadfb2ea8bf106a06f285cf
Ryan <ryandahlke@gmail.com>
2014-12-29 16:03:43 -0600

    pin in app.pins

--- static/js/app.js
+++ static/js/app.js
@@ -3,5 +3,6 @@ var app = angular.module("app", []);
 app.controller("AppCtrl", function () {
     var app = this;
 
-    app.message = "Am I working?";
+<div ng-repeat="pin in app.pins">
+    {{pin.title}}
 })
