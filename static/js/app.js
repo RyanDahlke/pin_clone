a396f28b172d9e877ade6367ee1a2ae00aba50aa
Ryan <ryandahlke@gmail.com>
2014-12-29 16:17:58 -0600

    add add pin button

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,17 @@
 var app = angular.module("app", []);
 
-app.controller("AppCtrl", function () {
+app.controller("AppCtrl", function ($http) {
     var app = this;
 
-<div ng-repeat="pin in app.pins">
-    {{pin.title}}
+    $http.get("/api/pin").success(function (data) {
+        app.pins = data.objects;
+    })
+
+    app.addPin = function () {
+        $http.post("/api/pin", {"title":"new", "image":"http://placekitten.com/200/200/?image=" + app.pins.length})
+            .success(function (data) {
+                app.pins.push(data);
+            })
+    }
+
 })
